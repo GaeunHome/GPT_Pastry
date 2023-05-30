3df881cae9b06a8a3997b840c9dcd1cc889ecf52
hank1224 <92917339+hank1224@users.noreply.github.com>
2023-05-30 11:49:09 +0800

    checkout

--- Project/pastry/admin.py
+++ Project/pastry/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
-from .models import Pastry, Member, Order, OrderItem 
+from .models import Pastry, Order, OrderItem, Member
 
-# Register your models here.
 class PastryAdmin(admin.ModelAdmin):
-    list_display = ('CATEGORY_CHOICES', 'name', 'category', 'description', 'price')
+    list_display = ('name', 'category', 'price')
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'member', 'date', 'status', 'total_price')
+    list_filter = ('status',)
+    inlines = [
+        OrderItemInline,
+    ]
 
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('user', 'contact_number')
-
+    
 admin.site.register(Pastry, PastryAdmin)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Member, MemberAdmin)
-# admin.site.register(Member, MemberAdmin)
-# admin.site.register(Member, MemberAdmin)
